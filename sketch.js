293493318a7522167269d9bf0344068efe070d89
AdamLbrz <adam.lubrez@epitech.eu>
2025-04-29 18:17:26 +0200

    modify game 2

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let ball;
+let balls = [];
 let ring;
 let gravity = 0.6;
 let accelerationFactor = 1.05;
@@ -8,8 +9,10 @@ let prevBallPos;
 let gameOver = false;
 let animationFrame = 0;
 let inMenu = true;
+let currentGame = null;
 let music;
 let bounceSound;
+const maxBalls = 100;
 
 function preload() {
   music = loadSound('music.mp3');
@@ -17,12 +20,18 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
+  frameRate(60);
   angleMode(RADIANS);
   colorMode(HSB);
   music.loop();
   background(0);
   resetSketch();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
@@ -31,6 +40,14 @@ function draw() {
     return;
   }
 
+  if (currentGame === "Game1") {
+    drawGame1();
+  } else if (currentGame === "Game2") {
+    drawGame2();
+  }
+}
+
+function drawGame1() {
   if (gameOver) {
     drawGoodGameAnimation();
     return;
@@ -83,6 +100,83 @@ function draw() {
   }
 }
 
+function drawGame2() {
+  gravity = 0.4;
+  background(0);
+  translate(width / 2, height / 2);
+
+  ringRotation = (ringRotation + 0.02) % TWO_PI;
+
+  noFill();
+  strokeWeight(30);
+  stroke(255);
+  beginShape();
+  for (let angle = 0; angle < TWO_PI; angle += 0.01) {
+    if (angle < ringRotation || angle > (ringRotation + ringHoleAngle) % TWO_PI) {
+      let x = ring.radius * cos(angle);
+      let y = ring.radius * sin(angle);
+      vertex(x, y);
+    }
+  }
+  endShape(CLOSE);
+
+  fill(0);
+  noStroke();
+  arc(0, 0, ring.radius * 2, ring.radius * 2, ringRotation, ringRotation + ringHoleAngle);
+
+  for (let i = 0; i < balls.length; i++) {
+    let b = balls[i];
+    b.vy += gravity;
+    b.vx = constrain(b.vx, -maxSpeed, maxSpeed);
+    b.vy = constrain(b.vy, -maxSpeed, maxSpeed);
+    b.x += b.vx;
+    b.y += b.vy;
+
+    noStroke();
+    fill((hue + i * 30) % 360, 255, 255);
+    circle(b.x, b.y, b.r * 2);
+
+    let distance = sqrt(b.x * b.x + b.y * b.y);
+    if (distance + b.r > ring.radius) {
+      let angle = atan2(b.y, b.x);
+      if (angle > ringRotation && angle < (ringRotation + ringHoleAngle) % TWO_PI) {
+        b.vy += gravity;
+      } else {
+        bounceSound.play();
+        let normX = b.x / distance;
+        let normY = b.y / distance;
+        let dot = b.vx * normX + b.vy * normY;
+        b.vx -= 2 * dot * normX;
+        b.vy -= 2 * dot * normY;
+        b.vx *= accelerationFactor;
+        b.vy *= accelerationFactor;
+        let overlap = b.r + distance - ring.radius;
+        b.x -= normX * overlap;
+        b.y -= normY * overlap;
+      }
+    }
+
+    if (b.y > height / 2 + b.r) {
+      balls.splice(i, 1);
+      i--;
+      if (balls.length < maxBalls) {
+        balls.push(createBall());
+        balls.push(createBall());
+      }
+    }
+  }
+}
+
+function createBall() {
+  return {
+    x: random(-ring.radius / 2, ring.radius / 2),
+    y: random(-ring.radius / 2, ring.radius / 2),
+    vx: random(-3, 3),
+    vy: random(-3, 3),
+    r: 5
+  };
+}
+
 function resetSketch() {
   ball = {
     x: 0,
@@ -91,9 +185,14 @@ function resetSketch() {
     vy: random(-3, 3),
     r: 10
   };
+  balls = [
+    { x: 0, y: 0, vx: random(-3, 3), vy: random(-3, 3), r: 5 }
+  ];
   ring = {
     radius: 200
   };
+  ringRotation = 0;
+  ringHoleAngle = Math.PI / 4;
   background(0);
   prevBallPos = { x: ball.x, y: ball.y };
   gameOver = false;
@@ -107,7 +206,8 @@ function drawMenu() {
   textSize(50);
   text("Satisfying Bouncing Ball", width / 2, height / 3);
   textSize(30);
-  text("Press SPACE to Play", width / 2, height / 2);
+  text("Press 1 for Game 1", width / 2, height / 2);
+  text("Press 2 for Game 2", width / 2, height / 2 + 50);
 }
 
 function drawGoodGameAnimation() {
@@ -120,11 +220,15 @@ function drawGoodGameAnimation() {
 }
 
 function keyPressed() {
-  if (keyCode === 32) {
-    if (inMenu) {
-      inMenu = false;
-    } else {
-      resetSketch();
-    }
+  if (keyCode === 49) {
+    inMenu = false;
+    currentGame = "Game1";
+    resetSketch();
+  } else if (keyCode === 50) {
+    inMenu = false;
+    currentGame = "Game2";
+    resetSketch();
+  } else if (keyCode === 32) {
+    resetSketch();
   }
 }
